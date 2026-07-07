bc14ce37073111699b5eec50dfc7a37eca6f1db7
chnbsdan <chnbsdan@gmail.com>
2026-07-07 16:28:53 +0800

    Create footer.js

---
+++ src/templates/components/footer.js
@@ -0,0 +1,7 @@
+export function getFooter() {
+  return `<footer class="footer">
+  <div class="footer-content">
+    <p>Copyright ©2024-2025 <span class="footer-link" onclick="openAboutModal()">Hangdn nav</span>. All Rights Reserved.</p>
+  </div>
+</footer>`;
+}
