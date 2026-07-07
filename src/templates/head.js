044197ea1b25d15ec8dd5d542e15dd92d3a4647f
chnbsdan <chnbsdan@gmail.com>
2026-07-07 16:26:28 +0800

    Create head.js

---
+++ src/templates/head.js
@@ -0,0 +1,11 @@
+export function getHead() {
+  return `<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hangdn个人智能导航</title>
+  <link rel="icon" href="https://cdn.jsdelivr.net/gh/chnbsdan/cloudflare-workers-blog@master/themes/mya/files/hangdn.ico" type="image/x-icon">
+  <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
+</head>`;
+}
