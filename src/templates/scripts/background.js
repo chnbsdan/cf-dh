547334efeb382656ca925b9d5248ee87c29800a6
chnbsdan <chnbsdan@gmail.com>
2026-07-07 16:33:28 +0800

    Add background script for rotating background images

---
+++ src/templates/scripts/background.js
@@ -0,0 +1,17 @@
+export function getBackgroundScript() {
+  return `<script>
+function initBackgroundRotation() {
+  const bgImgs = document.querySelectorAll('.background-slide');
+  let bgIndex = 0;
+  
+  setInterval(() => {
+    bgImgs.forEach((img, i) => img.classList.toggle('active', i === bgIndex));
+    bgIndex = (bgIndex + 1) % bgImgs.length;
+  }, 10000);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initBackgroundRotation();
+});
+</script>`;
+}
