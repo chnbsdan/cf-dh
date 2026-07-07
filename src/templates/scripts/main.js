ded9eb40e7dd54c7b275b788c376e020c838e95e
chnbsdan <chnbsdan@gmail.com>
2026-07-07 21:06:19 +0800

    Update main.js

--- src/templates/scripts/main.js
+++ src/templates/scripts/main.js
@@ -796,7 +796,7 @@ async function downloadBackup() {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `kv_backup_${new Date().toISOString().slice(0,10)}.json`;
+    a.download = `kv_backup_$${new Date().toISOString().slice(0,10)}.json`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
